c6b92bf441832e26359fab408053a7ca4f89ac66
waiwaidongci <786958579@qq.com>
2026-08-17 12:58:44 +0800

    gold: team-collaboration-assignee-nil-002

--- internal/taskrules/assignee_format.go
+++ internal/taskrules/assignee_format.go
@@ -3,5 +3,8 @@ package taskrules
 import "strconv"
 
 func FormatAssigneeID(id *int64) string {
+	if id == nil {
+		return ""
+	}
 	return strconv.FormatInt(*id, 10)
 }
--- internal/taskrules/assignee_pointer.go
+++ internal/taskrules/assignee_pointer.go
@@ -1,6 +1,9 @@
 package taskrules
 
 func AssigneeFromID(id int64) *int64 {
+	if id <= 0 {
+		return nil
+	}
 	value := id
 	return &value
 }
